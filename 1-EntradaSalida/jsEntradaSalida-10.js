8ceebdd8279419ed7e2da6bc3a3d626819fbcb8d
alumno <alumno@utnfra.com.ar>
2020-02-06 21:52:09 -0300

    segunda clase con el ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,20 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+    //declarovariables
+    var importeingresado;
+    var importeparseado;
+    var monto;
+    var descuento;
+
+    //ingreso de datos
+    importeingresado = document.getElementById ("importe").value;
+    //parseo de datos
+    importeparseado = parseInt (importeingresado);
+    //sacamos el descuento del 25%
+   descuento = 0.25;
+   
+    monto = importeparseado * descuento;
+
+    
 }
