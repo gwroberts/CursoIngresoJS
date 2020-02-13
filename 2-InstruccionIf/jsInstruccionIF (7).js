bf68cb49f742cfaba5ba2b192b1c0279a8f402f9
alumno <alumno@utnfra.com.ar>
2020-02-13 19:03:19 -0300

    ejercicios7y8

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+var estadocivil;
+edad = document.getElementById ("edad").value;
+estadocivil = document.getElementById ("estadoCivil").value;	
+//empezamos con el if     
+        if (edad < 18 && estadocivil != "soltero"  ){
+            alert ("es muy pequeño para no ser soltero ")
+    
+         } else {
 
-	
-
+         }
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+var estadocivil;
+edad = document.getElementById ("edad").value;
+estadocivil = document.getElementById("estadoCivil").value;
+// condicion de if  
+        if ( edad < 18 && estadocivil != "soltero"){
+        
+           }   else {
+               alert (" es soltero y no es menor ");
+               
+             }
+        
 
 	
 
