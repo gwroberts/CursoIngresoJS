b6d7d973e8c18352f8b4993a86a4dffcebcfc44b
alumno <alumno@utnfra.com.ar>
2020-02-20 21:14:16 -0300

    ejercicios8y9

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,9 +4,33 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
+	var numero 
 	var respuesta='si';
+	while (respuesta){
+	
+		var numero = prompt ("ingrese un numero");
+		numero = parseInt (numero);
+		
+		while(isNaN(numero)){
+			numero = prompt ("Error numero incorrercto");	//valido el numero que se ingresa para poder operar
+			numero = parseInt (numero);
+		}	//cierro el while de la validacion
+			//opero
 
+		if (numero >=0){
+			positivo = positivo + numero;
+
+		}else{
+			negativo = negativo *numero;
+			contador = contador +1;
+				
+		}
+		 respuesta = confirm ("desea continuar?");	
+	}//cierro el while que contiene respuesta	 
+			
+
+
+	
 
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,46 @@
-function mostrar()
-{
+function mostrar() {
 
-	var contador=0;
+	var contador = 0;
 	// declarar variables
-	
-	var respuesta='si';
+	var numero;
+	var respuesta = 'si';
+	var maximo;
+	var minimo;
+	var primeraIteracion = true;
+	while (respuesta) {
+		numero = prompt(" ingrese un numero");					//valido datos que se ingresan con los 2 while
+		numero = parseInt(numero);
+		while (isNaN(numero)) {
+			numero = prompt("Error, ingrese un numero ");
+			numero = parseInt(numero);
+		}	//cierro while de validacion
+		// opero
+		if (primeraIteracion) {   //es para que la primera iteracion carga como maximo y minimo porque no puedo inicializarlo
+			primeraIteracion = false;
+			minimo = numero;
+			maximo = numero;
+			contador = contador + 1;
+		} else {
 
-	while(respuesta!='no')
-	{
-		
-	
-	}
+
+			if (numero >= maximo) {
+				maximo = numero;
+			} else {
+				if (numero <= minimo) {
+					minimo = numero;
+
+				}
+			}
+		}
 
 
 
+		respuesta = confirm("desea continuar?");
+
+	}// cierro llave del while respuesta
+
+
+	document.getElementById('maximo').value =maximo;
+	document.getElementById('minimo').value = minimo;
 
 }//FIN DE LA FUNCIÓN
